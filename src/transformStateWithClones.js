9144840de28e19d7eaca38398e1b4d8a9e216bfa
Volodymyr Pavlenko <32794096+solaryasha@users.noreply.github.com>
2023-03-30 15:27:39 +0300

    tests was completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,35 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let currentState = { ...state };
+  const result = [];
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        currentState = {
+          ...currentState, ...action.extraData,
+        };
+        break;
+
+      case 'removeProperties':
+        currentState = Object.keys(currentState)
+          .filter(key => !action.keysToRemove.includes(key))
+          .reduce((newState, key) => {
+            newState[key] = currentState[key];
+
+            return newState;
+          }, {});
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+    }
+    result.push(currentState);
+  });
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
